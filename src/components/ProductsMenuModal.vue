8c1fb3ff0c029c7b4f7222c36aa81dc0241d7b8a
Arcgel <darkdusty8000@gmaio.com>
2024-11-19 21:02:26 +0800

    Template for design

--- src/components/ProductsMenuModal.vue
+++ src/components/ProductsMenuModal.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="containerProducts">
-    <h1>Products</h1>
+    <h1>PRODUCTS</h1>
   <div class="Error" v-if="error">{{ error }}</div>
   <div class="Products" v-if="products.length">
     <div v-for="product in products" :key="product.id">
       <div class="productShowcase">
-        {{ product.title }}
-        {{ product.Sizez }}
+        <h2>{{ product.title }}</h2>
+        <span v-for="sizes in product.Sizez" :key="sizes">
+          <h5>{{ sizes }}</h5>
+        </span>
+        <span v-for="config in product.Setting" :key="config">
+          <h5>{{ config }}</h5>
+        </span>
       </div>
     </div>
   </div>
@@ -17,7 +22,7 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref } from 'vue'
 const products = ref([])
 const error = ref(null)
 
@@ -36,7 +41,7 @@ const error = ref(null)
   }
 }
 
-load();
+load()
 
 </script>
 
@@ -59,10 +64,18 @@ load();
   }
   .productShowcase{
     width: 17vw;
-    height: 30vh;
+    height: 40vh;
     padding: 2vh;
     border-radius: 1vw;
     background-color: chocolate;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    box-shadow: 0 0.4vh 0.6vh rgba(0, 0, 0, 0.8);
+  }
+  .productShowcase h1,h2,h3,h4,h5,h6 {
+    padding: 0;
+    margin: 0;
   }
 
 </style>
